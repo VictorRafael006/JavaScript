465321b487f29df29718dfcd2690118498be1d41
Victor0006 <136948562+VictorRafael006@users.noreply.github.com>
2023-09-14 19:53:48 -0300

    Add files via upload

---
+++ Exercício-4/tempo.js
@@ -0,0 +1,14 @@
+function main()
+{
+ var num = parseInt(document.getElementById("num").value);
+ var resp = document.getElementById("resposta");
+ 
+ if(document.getElementById("CtoF").checked==true)
+  resp.innerHTML = ctof(num);
+
+}
+function ctof(x)
+{
+ return (1.8*x + 32);
+}
+
